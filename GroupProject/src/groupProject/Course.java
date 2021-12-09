383f64347b248d521b6a8c18f68eb64e57cf9a7f
Waleed Farooq <wfaro001@fiu.edu>
2021-12-09 16:34:44 -0500

    Added Course and School

--- GroupProject/src/groupProject/Course.java
+++ GroupProject/src/groupProject/Course.java
@@ -8,5 +8,25 @@ package groupProject;
  *
  */
 public class Course {
+	
+	private String courseCode;
+	private String courseTitle;
+	private String courseDescription;
+	private String schoolCode;
+	private SchoolPersonnel courseSupervisor;
+	private String teamID;
+	private float courseStatus;
+	
+	public void signUp() {
+		System.out.println("Team signed up to teach this course");
+	}
+	
+	public void courseStatusCheck() {
+		System.out.println("Course has been completed this much: ");
+	}
+	
+	public void postCourse() {
+		System.out.println("School has posted this course to be taught");
+	}
 
 }
--- GroupProject/src/groupProject/School.java
+++ GroupProject/src/groupProject/School.java
@@ -9,4 +9,21 @@ package groupProject;
  */
 public class School {
 
+	private String schoolCode;
+	private String schoolName;
+	private String phoneNumber;
+	private String address;
+	private String city;
+	private String state;
+	private String zipCode;
+	private boolean isActive;
+	
+	public void createSchool() {
+		System.out.println("School was created");
+	}
+	
+	public void editSchool() {
+		System.out.println("School was edited");
+		
+	}
 }
